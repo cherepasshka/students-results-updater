f30901564b814c5751df7225ff8621b7e5e13899
Darya Zhukova <cherepashka2080@gmail.com>
2021-11-10 01:10:21 +0300

    renamed prog, that execute scripts

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ res_made = os.system('python3 maketable.py > out.txt')
 if res_made != 0:
 	print('Something went wrong while generating data')
 	exit(1)
-sheet_built = os.system('python3 main.py')
+sheet_built = os.system('python3 writer_to_table.py')
 
